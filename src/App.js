7a2b4e527c469ceff2590e439dda1cd22600f194
Özgür  Toprak <zgrtprk@gmail.com>
2022-09-25 12:47:40 +0200

    design added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React from "react";
-import { Swith, Route, Link } from "react-router-dom";
+import { Swith, Route, Link, Switch } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components/";
+import { Navbar, Exchanges, Homepage, News, CryptoDetails, CryptoCurrencies } from "./components/";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-      <nav className="navbar">
+      <div className="navbar">
         <Navbar />
-      </nav>
-      <main className="main"></main>
-      <footer className="footer"></footer>
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <CryptoCurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
+            Cryptoworld <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
