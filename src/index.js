f4edb17f8ba3536f61880f7ab3e111550e46a253
Özgür  Toprak <zgrtprk@gmail.com>
2022-09-25 14:56:00 +0200

    end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 
-import App from "./App";
-import "antd/dist/antd.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <App />
-  </Router>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
